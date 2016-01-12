b8089c46f3d612d9fb130b8d8bb0a559370da643
Matt Klein <shesso8bit@gmail.com>
2016-01-11 19:48:42 -0500

    added ability to add new wine through webserver, unit tests for new location, and missing data page

--- salido_rails_challenge/app/controllers/products.rb
+++ salido_rails_challenge/app/controllers/products.rb
@@ -1,11 +1,36 @@
 SalidoRailsChallenge::App.controllers :products do
   get :index do
-    WineApi.new.pull_and_insert('155bc41216c9b981b8a05cee921f4fb1')
+    # WineApi.new.pull_and_insert('155bc41216c9b981b8a05cee921f4fb1')
 
     @wines = Product.all
-
 
     render 'products_index'
   end
 
+  get :new do
+  	render 'products_add'
+  end
+
+  post :add, :csrf_protection => false do
+  	wine_name = params['wine_name']
+  	wine_url = params['wine_url']
+  	min_price = params['wine_minprice']
+  	max_price = params['wine_maxprice']
+  	retail_price = params['wine_retailprice']
+  	wine_type = params['wine_type']
+
+  	if wine_name.present? && wine_url.present? && min_price.present? && max_price.present? && retail_price && wine_type.present?
+  		Product.create(
+        name: wine_name,
+        url: wine_url,
+        pricemin: min_price,
+        pricemax: max_price,
+        priceretail: retail_price,
+        winetype: wine_type
+      )
+      redirect_to :products
+  	else
+  		render 'products_missing'
+  	end
+  end
 end
--- salido_rails_challenge/spec/app/controllers/products_controller_spec.rb
+++ salido_rails_challenge/spec/app/controllers/products_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe '/products' do
 
-  describe '#GET' do
+  describe '#index' do
     context 'when the page is rendered' do
       it 'returns a 200' do
         sample_json = {"Status":{"Messages":[],"ReturnCode":0},"Products":{"List":[{"Id":120039,"Name":"90+ Point Red Wine Trio","Url":"http:\/\/www.wine.com\/v6\/90-Point-Red-Wine-Trio\/gift\/120039\/Detail.aspx","Appellation":"","Labels":[{"Id":"120039m","Name":"thumbnail","Url":"http:\/\/cache.wine.com\/labels\/120039m.jpg"}],"Type":"WineSet","Varietal":"","Vineyard":"","Vintage":"","Community":{"Reviews":{"HighestScore":5,"List":[],"Url":"http:\/\/www.wine.com\/v6\/90-Point-Red-Wine-Trio\/gift\/120039\/Detail.aspx?pageType=reviews"},"Url":"http:\/\/www.wine.com\/v6\/90-Point-Red-Wine-Trio\/gift\/120039\/Detail.aspx"},"Description":"Test Description","GeoLocation":{"Latitude":-360,"Longitude":-360,"Url":""},"PriceMax":49.9900,"PriceMin":49.9900,"PriceRetail":49.9900,"ProductAttributes":[{"Id":0,"Name":"Wine Gift Sets","Url":"","ImageUrl":""},{"Id":0,"Name":"Best Sellers","Url":"","ImageUrl":""},{"Id":0,"Name":"Colleagues and Clients","Url":"","ImageUrl":""},{"Id":0,"Name":"For Him","Url":"","ImageUrl":""},{"Id":0,"Name":"Business Gifts","Url":"","ImageUrl":""},{"Id":0,"Name":"1-4 Bottles","Url":"","ImageUrl":""},{"Id":0,"Name":"Thank You","Url":"","ImageUrl":""},{"Id":0,"Name":"Has Large Label","Url":"","ImageUrl":""}],"Ratings":{"HighestScore":0,"List":[]},"Retail":"","Vintages":{"List":[]}}],"Offset":0,"Total":89950,"Url":""}}.deep_stringify_keys.to_json
@@ -16,6 +16,24 @@ describe '/products' do
         get '/products', {}
 
         expect(last_response.status).to eq(200)
+      end
+    end
+  end
+
+  describe '#new' do
+    context 'when the page is rendered' do
+      it 'returns a 200' do
+        get '/products/new', {}
+
+        expect(last_response.status).to eq(200)
+      end
+
+      it 'returns some valid content' do
+        content = "Add a Wine"
+
+        get '/products/new', {}
+
+        expect(last_response.body).to include(content)
       end
     end
   end
